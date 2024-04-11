ec73c6a7c6c8252edf2910a15bd687f6feddf277
kade <bhpark93@gmail.com>
2024-04-12 03:21:26 +0900

    feat/ add new weather animation

--- level/gamelevel/Routine.gd
+++ level/gamelevel/Routine.gd
@@ -7,7 +7,7 @@ const extension = ".aseprite"
 var display_state: bool = true
 
 func change_texture(weather):
-	var file_name :String = weather + str(GameManger.current_round/4 +1)
+	var file_name :String = str(GameManger.current_round/4 +1) + weather
 	self.texture = load(path + file_name + extension)
 	
 	display_state = true
