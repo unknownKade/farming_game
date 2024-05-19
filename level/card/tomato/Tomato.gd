4d51b4c0c05c06e2b8144bfd19757ac60ec14def
kade <bhpark93@gmail.com>
2024-05-20 00:49:52 +0900

    fix/ seeding animation repeats after seeding

--- level/card/tomato/Tomato.gd
+++ level/card/tomato/Tomato.gd
@@ -21,6 +21,5 @@ func _on_deck_animation_player_animation_finished(anim_name):
 
 func _on_animation_player_animation_finished(anim_name):
 	if anim_name == "seeding":
-		anim_player.play("reset")
 		$Ground.visible = false
 		confirmed_card.emit()
