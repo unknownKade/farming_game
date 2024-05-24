617acff191aec60c209ea2dd527a85f2dabe386d
kade <bhpark93@gmail.com>
2024-05-25 06:39:19 +0900

    FARM-34 feat/ add opponent player bot - added carrot skill to opponent player bot

--- level/gamelevel/Hand.gd
+++ level/gamelevel/Hand.gd
@@ -13,6 +13,7 @@ func _ready():
 		child.card_return_ended.connect(end_return_card)
 
 func carrot_escape():
+	GameManger.carrot_escaped = true
 	if GameManger.selected_card == null:
 		$Carrot.select_card()
 		await get_tree().create_timer(1).timeout
@@ -22,22 +23,26 @@ func carrot_escape():
 	GameManger.player_turn = true
 	
 func swap_to_carrot():
+	%Board.get_node("Timer").stop()
 	if GameManger.selected_card == null:
 		get_node(GameManger.confirmed_card.name).select_card()
-		await get_tree().create_timer(3).timeout
-	get_node(GameManger.confirmed_card.name).deselect()
+		await get_tree().create_timer(2).timeout
+	get_node(GameManger.confirmed_card.name).deselect_card()
 	
+	await get_tree().create_timer(1).timeout
 	$Carrot.select_card()
+	await get_tree().create_timer(1).timeout
 	$Carrot.confirm_card()
-
+	GameManger.confirmed_card = GameManger.p1_deck['Carrot']
+	
 func display_cards():
 	for card in get_children() :
 		card.sync_card_level()
 	if GameManger.current_round%4 == 1 and !GameManger.carrot_escaped:
 		%HandAnimationPlayer.play("display")
 
-func play_confirm_card_animation(card : Crop):
-	get_node(card.name).confirm_card()
+func play_confirm_card_animation():
+	get_node(GameManger.confirmed_card.name).confirm_card()
 	
 func end_board_phase():
 	confirmed_card.emit()
--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -30,7 +30,7 @@ func _ready():
 		p1_deck[crop] = crops[crop].new()
 		p2_deck[crop] = crops[crop].new()
 	
-	p1_deck["Carrot"].level = 4
+	p2_deck["Beet"].level = 4
 	
 func get_random_card(deck) -> Crop:
 	var live_crops: Array
@@ -58,33 +58,37 @@ func confirm_card() :
 
 	#get player2 card with carrot checked
 	get_p2_card(get_random_card(p2_deck))
-	check_carrot_skill()
-
-func check_carrot_skill():
-	var p1 = confirmed_card
-	var p2 = opponent_card
+	opponent_card = p2_deck['Potato']
+	print(p2_deck['Potato'].level)
+	check_carrot_skill(true).emit()
+		
+func check_carrot_skill(is_host :bool):
+	var p1  = confirmed_card if is_host else opponent_card
+	var p2 = opponent_card if is_host else confirmed_card
+	var deck = p1_deck if is_host else p2_deck
 	var carrot_condition = p1 is Carrot and p1.level > 1 and p2 is Tomato
-	var beet_swap_condition = p2 is Beet and p1_deck[Crop.carrot].level > 1
-	var potato_condition = p1_deck["Potato"].state == Crop.States.PHOBIA
+	var beet_swap_condition = !(p1 is Carrot) and p2 is Beet and deck[Crop.carrot].level > 1
+	var potato_condition = deck["Potato"].state == Crop.States.PHOBIA
 
 	if !carrot_condition and !beet_swap_condition or potato_condition: 
-		confirmed_selected_card.emit(p1)
+		return confirmed_selected_card
 	elif beet_swap_condition :
-		swapped_to_carrot.emit()
+		return swapped_to_carrot
 	elif !p1.skill(p2):
-		confirmed_selected_card.emit(p1)
+		return confirmed_selected_card
 	else :
-		carrot_escaped = true
-		swapped_from_carrot.emit()
+		return swapped_from_carrot
 
 func p2_result(crop : Crop, value : int, message = null):
 	crop.grow_card(value)
 
-func get_p2_card(selected_card : Crop):
-	#if !test_crop.is_empty() :
-		#opponent_card = p2_deck[test_crop]
-	#else :
-		opponent_card = selected_card
+func get_p2_card(p2 : Crop):
+	opponent_card = p2
+	check_carrot_skill(false)
 	
-	# check_carrot_skill(opponent_card, confirmed_card) :
-	#	Crop.carrot : opponent_card = p2_deck[Crop.carrot]
+	match check_carrot_skill(false) :
+		swapped_from_carrot :
+			p2_deck[Crop.carrot].state = Crop.States.LOCKED
+			opponent_card = get_random_card(p2_deck)
+		swapped_to_carrot :
+			opponent_card = p2_deck[Crop.carrot]
