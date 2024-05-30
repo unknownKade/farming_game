f1ded9f2cc0ac02094f5cff639c66b777f12157c
kade <bhpark93@gmail.com>
2024-05-31 04:42:50 +0900

    rename/ orgainzed files, added font files, deleted unused scripts

---
+++ level/mainmenu/MainMenu.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+const game_path = "res://level/gamelevel/GameLevel.tscn"
+const rule_path = "res://level/Rule.tscn"
+func _ready():
+	pass
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file(game_path)
+
+func _on_rule_button_pressed():
+	get_tree().change_scene_to_file(rule_path)
+
+func _on_exit_button_pressed():
+	pass
