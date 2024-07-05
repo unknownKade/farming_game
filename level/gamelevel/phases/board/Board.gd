c3aa35d608050d05f43aad09220356a4f41495ef
kade <bhpark93@gmail.com>
2024-07-06 03:33:56 +0900

    FARM-71 feat/ timer on screen

--- level/gamelevel/phases/board/Board.gd
+++ level/gamelevel/phases/board/Board.gd
@@ -6,21 +6,25 @@ const next_state = "Garden"
 
 var hand : Dictionary
 var is_swap : bool = false
-	
+@export var timer : Node
+
 func _ready():
 	%Hand.get_node("Carrot").carrot_escaped.connect(enter)
 
 func enter():
 	%Hand.confirmed_card.connect(exit)
 	%Hand.display_cards()
-	$Timer.start()
+	timer.start()
 	GameManger.player_turn = true
-	
+
 func _on_timer_timeout():
-	$Timer.stop()
+	timer.stop()
 	if GameManger.player_turn:
 		GameManger.confirm_card()
 
 func exit():
-	$Timer.stop()
+	timer.stop()
 	Transition.emit(self, next_state)
+
+func get_timer_time() -> String:
+	return str(roundi(timer.time_left))
