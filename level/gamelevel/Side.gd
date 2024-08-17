8f6b45974d64c029ad47b7adfc2350e80d86b3ed
kade <bhpark93@gmail.com>
2024-08-18 03:11:32 +0900

    fix/ fix side menu icon and pause

--- level/gamelevel/Side.gd
+++ level/gamelevel/Side.gd
@@ -2,12 +2,15 @@ extends Node2D
 
 func _on_home_button_pressed():
 	SoundManager.sfx_play(&"menu_click")
-	SoundManager.bgms[SoundManager.current_bgm].stop()
-	get_tree().change_scene_to_file(ContentManager.menu_scene_path)
+	ContentManager.to_home()
 
 func _on_pause_button_pressed():
 	SoundManager.sfx_play(&"menu_click")
-	get_tree().paused = true
+	if get_tree().paused :
+		get_tree().paused = false
+	else :
+		get_parent().get_node("CanvasLayer").visible = true
+		get_tree().paused = true
 
 func _on_option_button_pressed():
 	SoundManager.sfx_play(&"menu_click")
