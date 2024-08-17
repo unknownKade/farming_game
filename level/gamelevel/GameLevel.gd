b9d99b92d138dede2ade63f8c51da94c3368f84e
kade <bhpark93@gmail.com>
2024-08-18 03:10:15 +0900

    feat/ add info on start and pause

--- level/gamelevel/GameLevel.gd
+++ level/gamelevel/GameLevel.gd
@@ -11,23 +11,21 @@ var current_state
 var states : Dictionary
 
 func _ready():
-	$White/AnimationPlayer.play("fade_in")
+	$CanvasLayer/AnimationPlayer.play("fade_in")
 	
-	await get_tree().create_timer(1).timeout
-	SoundManager.bgms[SoundManager.current_bgm].play()
 	GameManger.current_round = test_year * 4 - 4 + 1
+	current_state = initial_state
 	
 	for child in get_children():
 		if child is State:
 			states[child.name.to_lower()] = child
 			child.Transition.connect(on_child_transition)
-			
-	if initial_state:
-		initial_state.enter()
-		current_state = initial_state
 
 func on_child_transition(state, next_state):
 	if state != current_state:
+		print("current_state: {}", current_state)
+		print("state: {}", state)
+		print("next state : {}", next_state)
 		return
 		
 	if state == last_state:
@@ -37,3 +35,16 @@ func on_child_transition(state, next_state):
 	
 	current_state = new_state
 	new_state.enter()
+	
+func _on_instruction_instruction_clicked():
+	if current_state == initial_state:
+		initial_state.enter()
+	
+	if get_tree().paused :
+		$CanvasLayer.hide()
+		get_tree().paused = false
+
+func _on_animation_player_animation_finished(anim_name):
+	await get_tree().create_timer(1).timeout
+	$CanvasLayer/Instruction.show()
+	SoundManager.play_bgm()
