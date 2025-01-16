7a16c21b27d0257fbfe8f9d8549160ba587998f0
kade <bhpark93@gmail.com>
2025-01-17 04:15:54 +0900

    animate environment and season sometimes

--- level/gamelevel/phases/intro/Environment.gd
+++ level/gamelevel/phases/intro/Environment.gd
@@ -1,29 +1,35 @@
 extends Node2D
 
+@onready var anim_player: AnimationPlayer = get_node("AnimationPlayer")
 var scenes : Dictionary
 var environments : Array
+var current_environment
 
-func _ready():
+func _ready() -> void:
 	for child in get_children():
 		if child is Sprite2D and child.name != "Frame":
 			environments.append(child.name)
 			scenes[child.name] = child
 
-func enter():
+func enter() -> void:
 	next_environment()
-	#get_node(GameManger.animation_player).play("rain")
+	$Timer.start(randi_range(5,10))
 
-func next_environment():
+func next_environment() -> void:
 	reset()
-	var current_environment = environments[randi_range(0, environments.size()-1)]
+	current_environment = environments[randi_range(0, environments.size()-1)]
 
 	GameManger.current_environment = current_environment
 	var scene = scenes.get(current_environment)
 	
 	scene.visible = true
 	
-	get_node(GameManger.animation_player).queue(current_environment.to_lower())
+	get_node(GameManger.animation_player).play(current_environment.to_lower())
 
 func reset():
 	for scene in scenes :
 		scenes[scene].visible = false
+
+func _on_timer_timeout() -> void:
+	anim_player.play(current_environment.name.to_lower())
+	$Timer.start(randi_range(5,10))
--- level/gamelevel/phases/intro/Season.gd
+++ level/gamelevel/phases/intro/Season.gd
@@ -5,12 +5,19 @@ class_name Season
 signal end_weather
 
 @export var seasons: Array[Sprite2D]
+@onready var anim_player: AnimationPlayer = get_node("AnimationPlayer")
+var current_season
 
-func enter():
+func enter() -> void:
 	for season in get_children():
 		if season is Node2D:
 			season.visible = false
-	var current_season = seasons[GameManger.current_round%4 -1]
+	
+	current_season = seasons[GameManger.current_round%4 -1]
 	current_season.visible = true
-	SoundManager.sfx_play(&"weather_side")
-	get_node(GameManger.animation_player).play(current_season.name.to_lower())
+	anim_player.play(current_season.name.to_lower())
+	$Timer.start(randi_range(5,10))
+
+func _on_timer_timeout() -> void:
+	anim_player.play(current_season.name.to_lower())
+	$Timer.start(randi_range(5,10))
